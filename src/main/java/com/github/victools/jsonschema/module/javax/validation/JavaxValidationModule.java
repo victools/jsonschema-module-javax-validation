c86842f9dc5f51334f6d24c668f6e937b9f09a58
Carsten Wickner <Carsten.Wickner@gmx.de>
2019-09-01 21:22:45 +0200

    Support javax.validation groups

--- src/main/java/com/github/victools/jsonschema/module/javax/validation/JavaxValidationModule.java
+++ src/main/java/com/github/victools/jsonschema/module/javax/validation/JavaxValidationModule.java
@@ -26,7 +26,9 @@ import java.lang.annotation.Annotation;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Email;
@@ -56,7 +58,8 @@ import javax.validation.constraints.Size;
  */
 public class JavaxValidationModule implements Module {
 
-    private final List<JavaxValidationOption> options;
+    private final Set<JavaxValidationOption> options;
+    private Set<Class<?>> validationGroups;
 
     /**
      * Constructor.
@@ -64,7 +67,29 @@ public class JavaxValidationModule implements Module {
      * @param options features to enable
      */
     public JavaxValidationModule(JavaxValidationOption... options) {
-        this.options = options == null ? Collections.emptyList() : Arrays.asList(options);
+        this.options = options == null ? Collections.emptySet() : new HashSet<>(Arrays.asList(options));
+        // by default: ignore validation groups
+        this.validationGroups = null;
+    }
+
+    /**
+     * Add validation groups to be considered.
+     * <ul>
+     * <li>Never calling this method will result in all annotations to be picked-up.</li>
+     * <li>Calling this without parameters will only consider those annotations where no groups are defined.</li>
+     * <li>Calling this with not-null parameters will only consider those annotations without defined groups or where at least one matches.</li>
+     * </ul>
+     *
+     * @param validationGroups validation groups to consider
+     * @return this module instance (for chaining)
+     */
+    public JavaxValidationModule forValidationGroups(Class<?>... validationGroups) {
+        if (validationGroups == null) {
+            this.validationGroups = null;
+        } else {
+            this.validationGroups = new HashSet<>(Arrays.asList(validationGroups));
+        }
+        return this;
     }
 
     @Override
@@ -110,12 +135,14 @@ public class JavaxValidationModule implements Module {
      * @param <A> type of annotation
      * @param member field or method to retrieve annotation instance from (or from a field's getter or getter method's field)
      * @param annotationClass type of annotation
+     * @param validationGroupsLookup how to look-up the associated validation groups of an annotation instance
      * @return annotation instance (or {@code null})
      * @see MemberScope#getAnnotation(Class)
      * @see FieldScope#findGetter()
      * @see MethodScope#findGetterField()
      */
-    protected <A extends Annotation> A getAnnotationFromFieldOrGetter(MemberScope<?, ?> member, Class<A> annotationClass) {
+    protected <A extends Annotation> A getAnnotationFromFieldOrGetter(MemberScope<?, ?> member, Class<A> annotationClass,
+            Function<A, Class<?>[]> validationGroupsLookup) {
         A annotation = member.getAnnotation(annotationClass);
         if (annotation == null) {
             MemberScope<?, ?> associatedGetterOrField;
@@ -128,6 +155,20 @@ public class JavaxValidationModule implements Module {
             }
             annotation = associatedGetterOrField == null ? null : associatedGetterOrField.getAnnotation(annotationClass);
         }
+        if (annotation != null) {
+            Class<?>[] associatedGroups = validationGroupsLookup.apply(annotation);
+            /*
+             * the annotation is deemed applicable in one of the following three cases:
+             * 1. Validation groups are specifically ignored (i.e. forValidationGroups() was never called or with null as only parameter)
+             * 2. No validation groups are specified on the annotation.
+             * 3. Some validation group(s) are specified on the annotation and at least one of them was provided via forValidationGroups().
+             */
+            if (this.validationGroups != null && associatedGroups.length > 0
+                    && Collections.disjoint(this.validationGroups, Arrays.asList(associatedGroups))) {
+                // ignore the looked-up annotation as it is not associated with one of the desired validation groups
+                annotation = null;
+            }
+        }
         return annotation;
     }
 
@@ -139,12 +180,12 @@ public class JavaxValidationModule implements Module {
      */
     protected Boolean isNullable(MemberScope<?, ?> member) {
         Boolean result;
-        if (this.getAnnotationFromFieldOrGetter(member, NotNull.class) != null
-                || this.getAnnotationFromFieldOrGetter(member, NotBlank.class) != null
-                || this.getAnnotationFromFieldOrGetter(member, NotEmpty.class) != null) {
+        if (this.getAnnotationFromFieldOrGetter(member, NotNull.class, NotNull::groups) != null
+                || this.getAnnotationFromFieldOrGetter(member, NotBlank.class, NotBlank::groups) != null
+                || this.getAnnotationFromFieldOrGetter(member, NotEmpty.class, NotEmpty::groups) != null) {
             // field is specifically NOT nullable
             result = Boolean.FALSE;
-        } else if (this.getAnnotationFromFieldOrGetter(member, Null.class) != null) {
+        } else if (this.getAnnotationFromFieldOrGetter(member, Null.class, Null::groups) != null) {
             // field is specifically null (and thereby nullable)
             result = Boolean.TRUE;
         } else {
@@ -173,12 +214,12 @@ public class JavaxValidationModule implements Module {
      */
     protected Integer resolveArrayMinItems(MemberScope<?, ?> member) {
         if (member.isContainerType()) {
-            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class);
+            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class, Size::groups);
             if (sizeAnnotation != null && sizeAnnotation.min() > 0) {
                 // minimum length greater than the default 0 was specified
                 return sizeAnnotation.min();
             }
-            if (this.getAnnotationFromFieldOrGetter(member, NotEmpty.class) != null) {
+            if (this.getAnnotationFromFieldOrGetter(member, NotEmpty.class, NotEmpty::groups) != null) {
                 return 1;
             }
         }
@@ -194,7 +235,7 @@ public class JavaxValidationModule implements Module {
      */
     protected Integer resolveArrayMaxItems(MemberScope<?, ?> member) {
         if (member.isContainerType()) {
-            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class);
+            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class, Size::groups);
             if (sizeAnnotation != null && sizeAnnotation.max() < 2147483647) {
                 // maximum length below the default 2147483647 was specified
                 return sizeAnnotation.max();
@@ -214,13 +255,13 @@ public class JavaxValidationModule implements Module {
      */
     protected Integer resolveStringMinLength(MemberScope<?, ?> member) {
         if (member.getType().isInstanceOf(CharSequence.class)) {
-            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class);
+            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class, Size::groups);
             if (sizeAnnotation != null && sizeAnnotation.min() > 0) {
                 // minimum length greater than the default 0 was specified
                 return sizeAnnotation.min();
             }
-            if (this.getAnnotationFromFieldOrGetter(member, NotEmpty.class) != null
-                    || this.getAnnotationFromFieldOrGetter(member, NotBlank.class) != null) {
+            if (this.getAnnotationFromFieldOrGetter(member, NotEmpty.class, NotEmpty::groups) != null
+                    || this.getAnnotationFromFieldOrGetter(member, NotBlank.class, NotBlank::groups) != null) {
                 return 1;
             }
         }
@@ -236,7 +277,7 @@ public class JavaxValidationModule implements Module {
      */
     protected Integer resolveStringMaxLength(MemberScope<?, ?> member) {
         if (member.getType().isInstanceOf(CharSequence.class)) {
-            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class);
+            Size sizeAnnotation = this.getAnnotationFromFieldOrGetter(member, Size.class, Size::groups);
             if (sizeAnnotation != null && sizeAnnotation.max() < 2147483647) {
                 // maximum length below the default 2147483647 was specified
                 return sizeAnnotation.max();
@@ -254,7 +295,7 @@ public class JavaxValidationModule implements Module {
      */
     protected String resolveStringFormat(MemberScope<?, ?> member) {
         if (member.getType().isInstanceOf(CharSequence.class)) {
-            Email emailAnnotation = this.getAnnotationFromFieldOrGetter(member, Email.class);
+            Email emailAnnotation = this.getAnnotationFromFieldOrGetter(member, Email.class, Email::groups);
             if (emailAnnotation != null) {
                 // @Email annotation was found, indicate the respective format
                 if (this.options.contains(JavaxValidationOption.PREFER_IDN_EMAIL_FORMAT)) {
@@ -277,12 +318,12 @@ public class JavaxValidationModule implements Module {
      */
     protected String resolveStringPattern(MemberScope<?, ?> member) {
         if (member.getType().isInstanceOf(CharSequence.class)) {
-            Pattern patternAnnotation = this.getAnnotationFromFieldOrGetter(member, Pattern.class);
+            Pattern patternAnnotation = this.getAnnotationFromFieldOrGetter(member, Pattern.class, Pattern::groups);
             if (patternAnnotation != null) {
                 // @Pattern annotation was found, return its (mandatory) regular expression
                 return patternAnnotation.regexp();
             }
-            Email emailAnnotation = this.getAnnotationFromFieldOrGetter(member, Email.class);
+            Email emailAnnotation = this.getAnnotationFromFieldOrGetter(member, Email.class, Email::groups);
             if (emailAnnotation != null && !".*".equals(emailAnnotation.regexp())) {
                 // non-default regular expression on @Email annotation should also be considered
                 return emailAnnotation.regexp();
@@ -301,15 +342,15 @@ public class JavaxValidationModule implements Module {
      * @see PositiveOrZero
      */
     protected BigDecimal resolveNumberInclusiveMinimum(MemberScope<?, ?> member) {
-        Min minAnnotation = this.getAnnotationFromFieldOrGetter(member, Min.class);
+        Min minAnnotation = this.getAnnotationFromFieldOrGetter(member, Min.class, Min::groups);
         if (minAnnotation != null) {
             return new BigDecimal(minAnnotation.value());
         }
-        DecimalMin decimalMinAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMin.class);
+        DecimalMin decimalMinAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMin.class, DecimalMin::groups);
         if (decimalMinAnnotation != null && decimalMinAnnotation.inclusive()) {
             return new BigDecimal(decimalMinAnnotation.value());
         }
-        PositiveOrZero positiveAnnotation = this.getAnnotationFromFieldOrGetter(member, PositiveOrZero.class);
+        PositiveOrZero positiveAnnotation = this.getAnnotationFromFieldOrGetter(member, PositiveOrZero.class, PositiveOrZero::groups);
         if (positiveAnnotation != null) {
             return BigDecimal.ZERO;
         }
@@ -325,11 +366,11 @@ public class JavaxValidationModule implements Module {
      * @see Positive
      */
     protected BigDecimal resolveNumberExclusiveMinimum(MemberScope<?, ?> member) {
-        DecimalMin decimalMinAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMin.class);
+        DecimalMin decimalMinAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMin.class, DecimalMin::groups);
         if (decimalMinAnnotation != null && !decimalMinAnnotation.inclusive()) {
             return new BigDecimal(decimalMinAnnotation.value());
         }
-        Positive positiveAnnotation = this.getAnnotationFromFieldOrGetter(member, Positive.class);
+        Positive positiveAnnotation = this.getAnnotationFromFieldOrGetter(member, Positive.class, Positive::groups);
         if (positiveAnnotation != null) {
             return BigDecimal.ZERO;
         }
@@ -346,15 +387,15 @@ public class JavaxValidationModule implements Module {
      * @see NegativeOrZero
      */
     protected BigDecimal resolveNumberInclusiveMaximum(MemberScope<?, ?> member) {
-        Max maxAnnotation = this.getAnnotationFromFieldOrGetter(member, Max.class);
+        Max maxAnnotation = this.getAnnotationFromFieldOrGetter(member, Max.class, Max::groups);
         if (maxAnnotation != null) {
             return new BigDecimal(maxAnnotation.value());
         }
-        DecimalMax decimalMaxAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMax.class);
+        DecimalMax decimalMaxAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMax.class, DecimalMax::groups);
         if (decimalMaxAnnotation != null && decimalMaxAnnotation.inclusive()) {
             return new BigDecimal(decimalMaxAnnotation.value());
         }
-        NegativeOrZero negativeAnnotation = this.getAnnotationFromFieldOrGetter(member, NegativeOrZero.class);
+        NegativeOrZero negativeAnnotation = this.getAnnotationFromFieldOrGetter(member, NegativeOrZero.class, NegativeOrZero::groups);
         if (negativeAnnotation != null) {
             return BigDecimal.ZERO;
         }
@@ -370,11 +411,11 @@ public class JavaxValidationModule implements Module {
      * @see Negative
      */
     protected BigDecimal resolveNumberExclusiveMaximum(MemberScope<?, ?> member) {
-        DecimalMax decimalMaxAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMax.class);
+        DecimalMax decimalMaxAnnotation = this.getAnnotationFromFieldOrGetter(member, DecimalMax.class, DecimalMax::groups);
         if (decimalMaxAnnotation != null && !decimalMaxAnnotation.inclusive()) {
             return new BigDecimal(decimalMaxAnnotation.value());
         }
-        Negative negativeAnnotation = this.getAnnotationFromFieldOrGetter(member, Negative.class);
+        Negative negativeAnnotation = this.getAnnotationFromFieldOrGetter(member, Negative.class, Negative::groups);
         if (negativeAnnotation != null) {
             return BigDecimal.ZERO;
         }
